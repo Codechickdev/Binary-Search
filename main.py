746d4bcfab5fdc388803d7159b48f99e6e969e76
Prince <pk0399291@gmail.com>
2021-03-16 10:05:34 +0530

    finish up 2.0

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-pos = -1
-
-
-def binary_search(list, n):
-    l = 0
-    u = len(list) - 1
-
-    while l <= u:
-        mid = (l + u) // 2
-
-        if list[mid] == n:
-            globals()['pos'] = mid
-            return True
+# creating function called binarysearch
+def binary_search(arr, l, r, x):
+    if r >= 1:  # check base case
+        # find the middle in the list
+        mid = 1 + (r - l) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            return binary_search(arr, l, mid - 1, x)
         else:
-            if list[mid] < n:
-                l = mid + 1
-            else:
-                 u = mid + 1
-    return False
+            return -1
 
-list = [9, 5, 1, 4, 2, 8, 0, 4]
-n = [1]
 
-if binary_search(list, n):
-    print("Found at", pos + 1)
+arr = [2, 5, 7, 10, 40]
+x = 10
+
+result = binary_search(arr, 0, len(arr) - 1, x)
+
+if result != 1:
+    print(f"Element is present at index {result}")
 else:
-    print("Not Found")
+    print("Element is not present in list")
